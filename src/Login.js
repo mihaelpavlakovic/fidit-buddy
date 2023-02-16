a79cb8ee4f15c38987614b02461868224b3a700a
Mihael <mihael.pavlakovic@gmail.com>
2023-02-16 10:37:42 +0100

    adds visual design for Login/Register components

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,111 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <h1 className="text-3xl font-bold underline">Login</h1>;
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Neispravan unos emaila")
+        .required("Email je obavezan"),
+      password: Yup.string()
+        .required("Lozinka je obavezna")
+        .min(8, "Lozinka je pre kratka - mora bit minimalno 8 znakova dugacka"),
+    }),
+
+    onSubmit: values => {
+      console.log("form submitted");
+      console.log(values);
+      navigate("/");
+    },
+  });
+
+  return (
+    <main className="h-screen items-center flex justify-center">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-white rounded-lg w-1/2 shadow-xl"
+      >
+        <div className="text-gray-700  p-20">
+          <h1 className="text-3xl pb-2">Prijavi se u svoj račun 👋</h1>
+          <p className="text-lg  text-gray-500">
+            Očekujete nove vijesti na postavljena pitanja student-mentoru?
+            Prijavite se i saznajte odgovore na ta pitanja.
+          </p>
+          <div className="mt-6 ">
+            {/* Email input field */}
+            <div className="pb-4">
+              <label
+                htmlFor="email"
+                className={`block text-sm pb-2 ${
+                  formik.touched.email && formik.errors.email
+                    ? "text-red-400"
+                    : ""
+                }`}
+              >
+                {formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : "Email"}
+              </label>
+
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-full focus:border-teal-500 focus:ring-teal-500"
+                type="email"
+                name="email"
+                placeholder="Unesi svoju email adresu"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* Password input field */}
+            <div className="pb-4">
+              <label
+                htmlFor="password"
+                className={`block text-sm pb-2 ${
+                  formik.touched.password && formik.errors.password
+                    ? "text-red-400"
+                    : ""
+                }`}
+              >
+                {formik.touched.password && formik.errors.password
+                  ? formik.errors.password
+                  : "Lozinka"}
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-full focus:border-teal-500 focus:ring-teal-500"
+                type="password"
+                name="password"
+                placeholder="Unesi svoju lozinku"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-teal-500 text-sm text-white py-3 mt-6 rounded-lg w-full"
+            >
+              Prijavi se!
+            </button>
+            <p className="text-center text-gray-500 text-sm mt-5">
+              Nemate kreiran račun? Možete ga kreirati{" "}
+              <a href="/registracija" className="text-teal-500 underline">
+                ovdje
+              </a>
+            </p>
+          </div>
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default Login;
--- src/Registration.js
+++ src/Registration.js
@@ -1,5 +1,169 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+
 const Registration = () => {
-  return <h1 className="text-3xl font-bold underline">Registracija</h1>;
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      jmbag: "",
+    },
+
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(40, "Ime mora sadržavat 40 znakova ili manje.")
+        .required("Ime je obavezno"),
+      email: Yup.string()
+        .email("Neispravan unos emaila")
+        .required("Email je obavezan"),
+      password: Yup.string()
+        .required("Lozinka je obavezna")
+        .min(8, "Lozinka je pre kratka - mora bit minimalno 8 znakova dugacka"),
+      jmbag: Yup.string()
+        .required("JMBAG je obavezan")
+        .min(10, "Mora sadržavat točno 10 znamenki")
+        .max(10, "Mora sadržavat točno 10 znamenki"),
+    }),
+
+    onSubmit: values => {
+      console.log("form submitted");
+      console.log(values);
+      navigate("/");
+    },
+  });
+
+  return (
+    <main className="h-screen items-center flex justify-center">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-white rounded-lg w-1/2 shadow-xl"
+      >
+        <div className="text-gray-700  p-20">
+          <h1 className="text-3xl pb-2">Kreiraj svoj račun 👋</h1>
+          <p className="text-lg  text-gray-500">
+            Novi ste i imate puno neodgovorenih pitanja u vezi novo upisanog
+            fakulteta? Nema problema, pridruži se našoj platformi i postavi sva
+            svoja pitanja student-mentorima.
+          </p>
+          <div className="mt-6 ">
+            {/* Name input field */}
+            <div className="pb-4">
+              <label
+                htmlFor="name"
+                className={`block text-sm pb-2 ${
+                  formik.touched.name && formik.errors.name
+                    ? "text-red-400"
+                    : ""
+                } `}
+              >
+                {formik.touched.name && formik.errors.name
+                  ? formik.errors.name
+                  : "Ime"}
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-full focus:border-teal-500 focus:ring-teal-500 "
+                type="text"
+                name="name"
+                placeholder="Unesi svoje ime"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* Email input field */}
+            <div className="pb-4">
+              <label
+                htmlFor="email"
+                className={`block text-sm pb-2 ${
+                  formik.touched.email && formik.errors.email
+                    ? "text-red-400"
+                    : ""
+                }`}
+              >
+                {formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : "Email"}
+              </label>
+
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-full focus:border-teal-500 focus:ring-teal-500"
+                type="email"
+                name="email"
+                placeholder="Unesi svoju email adresu"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* Password input field */}
+            <div className="pb-4">
+              <label
+                htmlFor="password"
+                className={`block text-sm pb-2 ${
+                  formik.touched.password && formik.errors.password
+                    ? "text-red-400"
+                    : ""
+                }`}
+              >
+                {formik.touched.password && formik.errors.password
+                  ? formik.errors.password
+                  : "Lozinka"}
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-full focus:border-teal-500 focus:ring-teal-500"
+                type="password"
+                name="password"
+                placeholder="Unesi svoju lozinku"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* JMBAG input field */}
+            <div className="pb-4">
+              <label
+                htmlFor="jmbag"
+                className={`block text-sm pb-2 ${
+                  formik.touched.jmbag && formik.errors.jmbag
+                    ? "text-red-400"
+                    : ""
+                }`}
+              >
+                {formik.touched.jmbag && formik.errors.jmbag
+                  ? formik.errors.jmbag
+                  : "JMBAG"}
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-full focus:border-teal-500 focus:ring-teal-500"
+                type="number"
+                name="jmbag"
+                placeholder="Unesi svoj JMBAG"
+                onChange={formik.handleChange}
+                value={formik.values.jmbag}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-teal-500 text-sm text-white py-3 mt-6 rounded-lg w-full"
+            >
+              Kreiraj račun!
+            </button>
+            <p className="text-center text-gray-500 text-sm mt-5">
+              Imate kreiran račun? Možete se prijaviti{" "}
+              <a href="/prijava" className="text-teal-500 underline">
+                ovdje
+              </a>
+            </p>
+          </div>
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default Registration;
