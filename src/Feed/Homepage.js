1ff1944ef17034abe3b2c18392037f8a8f8b6927
Mihael <mihael.pavlakovic@gmail.com>
2023-03-01 17:15:20 +0100

    adds remove functionality for posts and comments

--- src/Feed/Homepage.js
+++ src/Feed/Homepage.js
@@ -2,88 +2,131 @@ import Navigation from "../Navigation";
 import Post from "./Post";
 import { db } from "../firebase";
 import {
-	collection,
-	onSnapshot,
-	orderBy,
-	query,
-	getDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  getDoc,
+  doc,
+  arrayRemove,
+  updateDoc,
 } from "firebase/firestore";
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
-	const [docs, setDocs] = useState([]);
-	const { currentUser } = useContext(AuthContext);
-	let userIsAdmin = localStorage.getItem("isAdmin");
+  const { currentUser } = useContext(AuthContext);
+  let userIsAdmin = localStorage.getItem("isAdmin");
+  const [docs, setDocs] = useState([]);
 
-	useEffect(() => {
-		const collRef = collection(db, "posts");
-		const q = query(
-			collRef,
-			orderBy("createdDate", "desc"),
-			orderBy("createdTime", "desc")
-		);
-		const unsub = onSnapshot(q, (snap) => {
-			let documents = [];
-			snap.forEach((doc) => {
-				let documentObject = { ...doc.data(), docId: doc.id };
-				doc.data().user &&
-					getDoc(doc.data().user)
-						.then((res) => {
-							documentObject.user = res.data();
-							documents.push(documentObject);
-						})
-						.catch((err) => console.log(err));
-				doc.data().comments &&
-					documentObject.comments.forEach((item, index) => {
-						getDoc(item.user).then((usr) => {
-							documentObject.comments[index].user = usr.data();
-							setDocs(documents);
-						});
-					});
-			});
-		});
-		return () => unsub();
-	}, []);
+  const onDelete = async (comIndex, docId) => {
+    const docRef = doc(db, "posts", docId);
+    const docForDelete = await getDoc(docRef).then(doc => doc.data());
 
-	return (
-		<>
-			<Navigation />
-			<main className="xl:max-w-7xl xl:mx-auto max-w-full px-5 sm:px-[8%]">
-				<div className="flex flex-col">
-					<h1 className="text-3xl my-5 font-semibold">
-						Dobro došli {currentUser.displayName}! 👋
-					</h1>
-					{userIsAdmin === "true" ? (
-						<p className="mt-5">
-							Prijavljeni ste kao administrator. Kako bi vidjeli
-							administratorsku ploču kliknite{" "}
-							<Link to="/admin" className="text-teal-500 hover:underline">
-								ovdje
-							</Link>
-							.
-						</p>
-					) : docs.lenght === 0 ? (
-						<p className="mt-5">
-							Trenutačno nema objava. Kreirajte prvu objavu{" "}
-							<Link
-								to="/kreiraj-objavu"
-								className="text-teal-500 hover:underline"
-							>
-								ovdje
-							</Link>
-							.
-						</p>
-					) : (
-						docs.map((item) => {
-							return <Post key={item.docId} postDetail={item} />;
-						})
-					)}
-				</div>
-			</main>
-		</>
-	);
+    await updateDoc(docRef, {
+      comments: arrayRemove(docForDelete.comments[comIndex]),
+    })
+      .then(() => {
+        alert("Komentar je uspjesno obrisan.");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    const postsRef = collection(db, "posts");
+    const q = query(
+      postsRef,
+      orderBy("createdDate", "asc"),
+      orderBy("createdTime", "desc")
+    );
+
+    const unsubscribe = onSnapshot(q, snapshot => {
+      const updatedPosts = [];
+      snapshot.forEach(doc => {
+        const postData = doc.data();
+        const post = { docId: doc.id, ...postData };
+
+        if (postData.user) {
+          getDoc(postData.user).then(userDoc => {
+            post.user = userDoc.data();
+            setDocs(prevPosts =>
+              prevPosts.map(p => (p.docId === post.docId ? post : p))
+            );
+          });
+        }
+
+        if (postData.comments) {
+          Promise.all(
+            postData.comments.map(comment =>
+              getDoc(comment.user).then(doc => doc.data())
+            )
+          ).then(commentUsers => {
+            post.comments = postData.comments.map((comment, index) => ({
+              ...comment,
+              user: commentUsers[index],
+            }));
+            setDocs(prevPosts =>
+              prevPosts.map(p => (p.docId === post.docId ? post : p))
+            );
+          });
+        }
+
+        updatedPosts.push(post);
+      });
+
+      setDocs(updatedPosts);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <>
+      <Navigation />
+      <main className="xl:max-w-7xl xl:mx-auto max-w-full px-5 sm:px-[8%]">
+        <div className="flex flex-col">
+          <h1 className="text-3xl my-5 font-semibold">
+            Dobro došli {currentUser.displayName}! 👋
+          </h1>
+          {userIsAdmin === "true" ? (
+            <p className="mt-5">
+              Prijavljeni ste kao administrator. Kako bi vidjeli
+              administratorsku ploču kliknite{" "}
+              <Link to="/admin" className="text-teal-500 hover:underline">
+                ovdje
+              </Link>
+              .
+            </p>
+          ) : docs.lenght === 0 ? (
+            <p className="mt-5">
+              Trenutačno nema objava. Kreirajte prvu objavu{" "}
+              <Link
+                to="/kreiraj-objavu"
+                className="text-teal-500 hover:underline"
+              >
+                ovdje
+              </Link>
+              .
+            </p>
+          ) : (
+            docs.map(item => {
+              return (
+                <Post
+                  key={item.docId}
+                  postId={item.docId}
+                  postDetail={item}
+                  onDeleteHadnler={onDelete}
+                />
+              );
+            })
+          )}
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Homepage;
--- src/Feed/Post.js
+++ src/Feed/Post.js
@@ -1,139 +1,165 @@
 import PostComments from "./PostComments";
 import { FiSend } from "react-icons/fi";
 import { useFormik } from "formik";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
-const Post = ({ postDetail }) => {
-	const { currentUser } = useContext(AuthContext);
+const Post = ({ postDetail, postId, onDeleteHadnler }) => {
+  const { currentUser } = useContext(AuthContext);
+  const formik = useFormik({
+    initialValues: {
+      comment: "",
+    },
 
-	const formik = useFormik({
-		initialValues: {
-			comment: "",
-		},
+    onSubmit: async values => {
+      const userRef = doc(db, "users", currentUser.uid);
+      const docRef = doc(db, "posts", postDetail.docId);
+      const docSnap = await getDoc(docRef);
+      let createComment = {};
+      if (!docSnap.data().comments) {
+        createComment = {
+          ...docSnap.data(),
+          comments: [
+            {
+              comment: values.comment,
+              commentCreatedDate: new Date().toLocaleDateString("hr-HR"),
+              commentCreatedTime: new Date().toLocaleString("hr-HR", {
+                hour: "numeric",
+                minute: "numeric",
+              }),
+              user: userRef,
+            },
+          ],
+        };
+      } else {
+        createComment = {
+          ...docSnap.data(),
+          comments: [
+            ...docSnap.data().comments,
+            {
+              comment: values.comment,
+              commentCreatedDate: new Date().toLocaleDateString("hr-HR"),
+              commentCreatedTime: new Date().toLocaleString("hr-HR", {
+                hour: "numeric",
+                minute: "numeric",
+              }),
+              user: userRef,
+            },
+          ],
+        };
+      }
 
-		onSubmit: async (values) => {
-			const userRef = doc(db, "users", currentUser.uid);
-			const docRef = doc(db, "posts", postDetail.docId);
-			const docSnap = await getDoc(docRef);
-			let createComment = {};
-			if (!docSnap.data().comments) {
-				createComment = {
-					...docSnap.data(),
-					comments: [
-						{
-							comment: values.comment,
-							commentCreatedDate: new Date().toLocaleDateString("hr-HR"),
-							commentCreatedTime: new Date().toLocaleString("hr-HR", {
-								hour: "numeric",
-								minute: "numeric",
-							}),
-							user: userRef,
-						},
-					],
-				};
-			} else {
-				createComment = {
-					...docSnap.data(),
-					comments: [
-						...docSnap.data().comments,
-						{
-							comment: values.comment,
-							commentCreatedDate: new Date().toLocaleDateString("hr-HR"),
-							commentCreatedTime: new Date().toLocaleString("hr-HR", {
-								hour: "numeric",
-								minute: "numeric",
-							}),
-							user: userRef,
-						},
-					],
-				};
-			}
+      try {
+        updateDoc(docRef, createComment);
+        values.comment = "";
+        console.log("Comment created...");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
 
-			try {
-				updateDoc(docRef, createComment);
-				values.comment = "";
-				console.log("Comment created...");
-			} catch (error) {
-				console.log(error);
-			}
-		},
-	});
+  const deletePostHandler = async postId => {
+    const docRef = doc(db, "posts", postId);
 
-	return (
-		<div className="w-full lg:w-2/3 p-5 sm:px-10 shadow-lg mx-auto mb-10 drop-shadow-md rounded-lg">
-			<div className="flex flex-col gap-4">
-				<div className="flex gap-3 items-center">
-					<img
-						className="w-11 h-11 rounded-md"
-						src={postDetail.user.photoURL}
-						alt="Slika profila student-mentora"
-					/>
-					<div className="w-full">
-						<div className="flex items-center">
-							<h3 className="flex-auto font-semibold">
-								{postDetail.user.displayName}
-								{postDetail.user.studentMentor && (
-									<span className="text-xs font-normal ml-2 bg-gray-200 px-2 py-0.5 rounded-full">
-										mentor
-									</span>
-								)}
-							</h3>
-							<div className="text-xs text-gray-500">
-								{postDetail.createdDate} - {postDetail.createdTime}
-							</div>
-						</div>
-						<p className="flex text-sm text-gray-500">
-							{postDetail.user.email}
-						</p>
-					</div>
-				</div>
-				<div>
-					<h2 className="text-2xl font-semibold">{postDetail.postTitle}</h2>
-				</div>
-				<p className="text-gray-500">{postDetail.postText}</p>
-				{postDetail.data &&
-					Object.entries(postDetail.data).map((item, index) => {
-						if (item[1].includes(".pdf"))
-							return (
-								<a key={index} href={item[1]} target="_blank" rel="noreferrer">
-									Preuzmi dokument
-								</a>
-							);
-						return <img key={index} src={item[1]} alt={`Slika ${index}`} />;
-					})}
-				<div>
-					{!postDetail.comments
-						? "Nema komentara"
-						: postDetail.comments.map((item, index) => {
-								return <PostComments key={index} comments={item} />;
-						  })}
-				</div>
-				<form
-					onSubmit={formik.handleSubmit}
-					className="flex md:flex-row flex-col items-center gap-3"
-				>
-					<textarea
-						className="w-full md:w-5/6 p-2 text-gray-500 rounded-md border-2 focus:border-teal-500 focus:ring-teal-500 focus:outline-none"
-						rows="1"
-						type="text"
-						name="comment"
-						placeholder="Napiši svoj komentar"
-						onChange={formik.handleChange}
-						value={formik.values.comment}
-					></textarea>
-					<button
-						type="submit"
-						className="w-full md:w-1/6 bg-teal-500 text-sm text-white py-3 rounded-lg hover:bg-teal-600"
-					>
-						<FiSend className="w-full" />
-					</button>
-				</form>
-			</div>
-		</div>
-	);
+    await deleteDoc(docRef)
+      .then(() => {
+        alert("Objava uspjesno obrisana.");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="w-full lg:w-2/3 p-5 sm:px-10 shadow-lg mx-auto mb-10 drop-shadow-md rounded-lg">
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-11 h-11 rounded-md"
+            src={postDetail.user.photoURL}
+            alt="Slika profila student-mentora"
+          />
+          <div className="w-full">
+            <div className="flex items-center">
+              <h3 className="flex-auto font-semibold">
+                {postDetail.user.displayName}
+                {postDetail.user.studentMentor && (
+                  <span className="text-xs font-normal ml-2 bg-gray-200 px-2 py-0.5 rounded-full">
+                    mentor
+                  </span>
+                )}
+              </h3>
+              <div className="text-xs text-gray-500">
+                <div>
+                  {postDetail.createdDate} - {postDetail.createdTime}
+                </div>
+                <div className="text-right">
+                  {currentUser.uid === postDetail.user.uid && (
+                    <button onClick={() => deletePostHandler(postId)}>
+                      Izbriši
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+            <p className="flex text-sm text-gray-500">
+              {postDetail.user.email}
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold">{postDetail.postTitle}</h2>
+        </div>
+        <p className="text-gray-500">{postDetail.postText}</p>
+        {postDetail.data &&
+          Object.entries(postDetail.data).map((item, index) => {
+            if (item[1].includes(".pdf"))
+              return (
+                <a key={index} href={item[1]} target="_blank" rel="noreferrer">
+                  Preuzmi dokument
+                </a>
+              );
+            return <img key={index} src={item[1]} alt={`Slika ${index}`} />;
+          })}
+        <div>
+          {!postDetail.comments
+            ? "Nema komentara"
+            : postDetail.comments.map((item, index) => {
+                return (
+                  <PostComments
+                    key={index}
+                    comments={item}
+                    onDelete={() => onDeleteHadnler(index, postId)}
+                  />
+                );
+              })}
+        </div>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex md:flex-row flex-col items-center gap-3"
+        >
+          <textarea
+            className="w-full md:w-5/6 p-2 text-gray-500 rounded-md border-2 focus:border-teal-500 focus:ring-teal-500 focus:outline-none"
+            rows="1"
+            type="text"
+            name="comment"
+            placeholder="Napiši svoj komentar"
+            onChange={formik.handleChange}
+            value={formik.values.comment}
+          ></textarea>
+          <button
+            type="submit"
+            className="w-full md:w-1/6 bg-teal-500 text-sm text-white py-3 rounded-lg hover:bg-teal-600"
+          >
+            <FiSend className="w-full" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
